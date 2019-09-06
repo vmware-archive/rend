095b75767be704a4f294bfd039fa5deb680a635c
Thomas S Hatch <thatch45@gmail.com>
2019-09-05 21:13:24 -0600

    build.py is clean and works

--- build.py
+++ build.py
@@ -5,113 +5,151 @@ import shutil
 import subprocess
 import tempfile
 import venv
+import argparse
 
-NAME = 'rend'
-
-cwd = os.getcwd()
-run = os.path.join(cwd, 'run.py')
-def to_import(path): 
-    ret = path[path.index('site-packages') + 14:].replace(os.sep, '.')
-    if ret.endswith('.py'):
-        ret = ret[:-3]
-    return ret
+OMIT = ('__pycache__', 'PyInstaller', 'pip', 'setuptools', 'pkg_resources', '__pycache__', 'dist-info', 'egg-info')
 
 
-def to_data(path):
-    dest = path[path.index('site-packages') + 14:]
-    src = path
-    if not dest.strip():
-        return None
-    ret = f'{src}{os.pathsep}{dest}'
-    return ret
+def parse():
+    '''
+    Parse the cli args
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            'name',
+            help='The name of the script to build from the run.py')
+    args = parser.parse_args()
+    return args.__dict__
 
 
-# Make a virtual environment based on the version of python used to call this script
-venv_dir = tempfile.mkdtemp(prefix='pop_', suffix='_venv')
-venv.create(venv_dir, clear=True, with_pip=True)
-python_bin = os.path.join(venv_dir, 'bin', 'python')
-pip_bin = os.path.join(venv_dir, 'bin', 'pip')
+class Builder:
+    def __init__(self):
+        self.opts = parse()
+        self.name = self.opts['name']
+        self.cwd = os.getcwd()
+        self.run = os.path.join(self.cwd, 'run.py')
+        self.venv_dir = tempfile.mkdtemp(prefix='pop_', suffix='_venv')
+        self.python_bin = os.path.join(self.venv_dir, 'bin', 'python')
+        self.vroot = os.path.join(self.venv_dir, 'lib')
+        self.all_paths = set()
+        self.imports = set()
+        self.datas = set()
+        self.cmd = f'{self.python_bin} -B -OO -m PyInstaller '
+        self.s_path = os.path.join(self.venv_dir, 'bin', self.name)
+        self.pyi_args = [
+              self.s_path,
+              '--log-level=INFO',
+              '--noconfirm',
+              '--onefile',
+              '--clean',
+            ]
 
-# Install dependencies
-subprocess.call([pip_bin, 'install', '-r', 'requirements.txt'])
-subprocess.call([pip_bin, 'install', 'PyInstaller'])
-subprocess.call([pip_bin, '-v', 'install', cwd])
-vroot = os.path.join(venv_dir, 'lib')
-all_paths = set()
-for root, dirs, files in os.walk(vroot):
-    if 'PyInstaller' in root:
-        continue
-    if 'pip' in root:
-        continue
-    if 'setuptools' in root:
-        continue
-    if 'pkg_resources' in root:
-        continue
-    if '__pycache__' in root:
-        continue
-    for d in dirs:
-        full = os.path.join(root, d)
-        if '__pycache__' in full:
-            continue
-        if 'dist-info' in full:
-            continue
-        if 'egg-info' in full:
-            continue
-        all_paths.add(full)
-    for f in files:
-        full = os.path.join(root, f)
-        if '__pycache__' in full:
-            continue
-        if 'dist-info' in full:
-            continue
-        if 'egg-info' in full:
-            continue
-        all_paths.add(full)
-imports = set()
-datas = set()
-for path in all_paths:
-    if not 'site-packages' in path:
-        continue
-    if os.path.isfile(path):
-        if not path.endswith('.py'):
-            continue
-        if path.endswith('__init__.py'):
-            # Skip it, we will get the dir
-            continue
-        imp = to_import(path)
-        if imp:
-            imports.add(imp)
-    if os.path.isdir(path):
-        data = to_data(path)
-        imp = to_import(path)
-        if imp:
-            imports.add(imp)
-        if data:
-            datas.add(data)
+    def create(self):
+        '''
+        Make a virtual environment based on the version of python used to call this script
+        '''
+        venv.create(self.venv_dir, clear=True, with_pip=True)
+        pip_bin = os.path.join(self.venv_dir, 'bin', 'pip')
+        subprocess.call([pip_bin, 'install', '-r', 'requirements.txt'])
+        subprocess.call([pip_bin, 'install', 'PyInstaller'])
+        subprocess.call([pip_bin, '-v', 'install', self.cwd])
+
+    def omit(self, test):
+        for bad in OMIT:
+            if bad in test:
+                return True
+        return False
+
+    def scan(self):
+        '''
+        Scan the new venv for files and imports
+        '''
+        for root, dirs, files in os.walk(self.vroot):
+            if self.omit(root):
+                continue
+            for d in dirs:
+                full = os.path.join(root, d)
+                if self.omit(full):
+                    continue
+                self.all_paths.add(full)
+            for f in files:
+                full = os.path.join(root, f)
+                if self.omit(full):
+                    continue
+                self.all_paths.add(full)
+
+    def to_import(self, path): 
+        ret = path[path.index('site-packages') + 14:].replace(os.sep, '.')
+        if ret.endswith('.py'):
+            ret = ret[:-3]
+        return ret
+
+    def to_data(self, path):
+        dest = path[path.index('site-packages') + 14:]
+        src = path
+        if not dest.strip():
+            return None
+        ret = f'{src}{os.pathsep}{dest}'
+        return ret
+
+    def mk_adds(self):
+        '''
+        make the imports and datas for pyinstaller
+        '''
+        for path in self.all_paths:
+            if not 'site-packages' in path:
+                continue
+            if os.path.isfile(path):
+                if not path.endswith('.py'):
+                    continue
+                if path.endswith('__init__.py'):
+                    # Skip it, we will get the dir
+                    continue
+                imp = self.to_import(path)
+                if imp:
+                    self.imports.add(imp)
+            if os.path.isdir(path):
+                data = self.to_data(path)
+                imp = self.to_import(path)
+                if imp:
+                    self.imports.add(imp)
+                if data:
+                    self.datas.add(data)
+
+    def mk_cmd(self):
+        '''
+        Create the pyinstaller command
+        '''
+        for imp in self.imports:
+            self.pyi_args.append(f'--hidden-import={imp}')
+        for data in self.datas:
+            self.pyi_args.append(f'--add-data={data}')
+        for arg in self.pyi_args:
+            self.cmd += f'{arg} '
+
+    def pyinst(self):
+        shutil.copy(self.run, self.s_path)
+        subprocess.call(self.cmd, shell=True)
+
+    def report(self):
+        art = os.path.join(self.cwd, 'dist', self.name)
+        print(f'Executable created in {art}')
+
+    def clean(self):
+        shutil.rmtree(self.venv_dir)
+        shutil.rmtree(os.path.join(self.cwd, 'build'))
+        os.remove(os.path.join(self.cwd, f'{self.name}.spec'))
+
+    def build(self):
+        self.create()
+        self.scan()
+        self.mk_adds()
+        self.mk_cmd()
+        self.pyinst()
+        self.report()
+        self.clean()
 
 
-# Run pyinstaller from the virtual environment to create the script
-s_path = os.path.join(venv_dir, 'bin', NAME)
-shutil.copy(run, s_path)
-cmd = f'{python_bin} -B -OO -m PyInstaller '
-pyi_args = [
-          s_path,
-          '--log-level=INFO',
-          '--noconfirm',
-          '--onefile',
-          '--clean',
-        ]
-
-for imp in imports:
-    pyi_args.append(f'--hidden-import={imp}')
-for data in datas:
-    pyi_args.append(f'--add-data={data}')
-for arg in pyi_args:
-    cmd += f'{arg} '
-subprocess.call(cmd, shell=True)
-
-
-print(cmd)
-print(f'venv created in {venv_dir}')
-print('Executable created in {}'.format(os.path.join(cwd, 'dist', NAME)))
-#shutil.rmtree(venv_dir)
+if __name__ == '__main__':
+    builder = Builder()
+    builder.build()
